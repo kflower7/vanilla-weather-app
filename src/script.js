300d31098b780634bf82c2df6a4b29c0778397d5
kflower7 <113183998+kflower7@users.noreply.github.com>
2022-10-26 18:56:03 +0200

    Current Location API JS

--- src/script.js
+++ src/script.js
@@ -93,6 +93,236 @@ form.addEventListener("submit", cityName);
 
 // Current Location and data
 
+function currentLocation() {
+  navigator.geolocation.getCurrentPosition(getCityName);
+}
+
+function getCityName(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "a7083641b7ebf003aec9614bf223ec5f";
+  let apiUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  let forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCityName);
+  axios.get(forecastApiUrl).then(displayCurrentForecast);
+}
+
+function displayCityName(response) {
+  currentCityDisplay = document.querySelector("#current-city");
+  currentCityName = response.data[0].name.toUpperCase();
+  apiKey = "a7083641b7ebf003aec9614bf223ec5f";
+  unit = "metric";
+  let currentTemperatureUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&limit=1&units=${unit}&appid=${apiKey}`;
+
+  currentCityDisplay.innerHTML = `${currentCityName}`;
+  axios.get(currentTemperatureUrl).then(showCurrentTemperature);
+}
+
+function showCurrentTemperature(response) {
+  let currentMainTemp = document.querySelector("#main-temp");
+  let currentMax = document.querySelector("#main-high");
+  let currentMin = document.querySelector("#main-low");
+  let mainCelsiusTemperature = response.data.main.temp;
+  let mainCelsiusHigh = response.data.main.temp_max;
+  let mainCelsiusLow = response.data.main.temp_min;
+  let currentForecast = document.querySelector("#main-forecast");
+  let currentBackgroundImage = document.querySelector("#main-image");
+  let currentForecastImage = response.data.weather[0].main;
+  if (currentForecastImage === "Clear") {
+    currentForecastImage = "sunny";
+  }
+  if (currentForecastImage === "Clouds") {
+    currentForecastImage = "cloudy";
+  }
+  if (currentForecastImage === "Snow") {
+    currentForecastImage = "snowy";
+  }
+  if (currentForecastImage === "Rain") {
+    currentForecastImage = "rainy";
+  }
+
+  currentForecast.innerHTML = `${currentForecastImage}`;
+  currentMainTemp.innerHTML = Math.round(mainCelsiusTemperature);
+  currentMax.innerHTML = Math.round(mainCelsiusHigh);
+  currentMin.innerHTML = Math.round(mainCelsiusLow);
+  currentBackgroundImage.setAttribute(
+    "src",
+    `images/${currentForecastImage}.png`
+  );
+  currentBackgroundImage.setAttribute(
+    "alt",
+    response.data.weather[0].description
+  );
+
+  function mainFahrenheitConversion(event) {
+    event.preventDefault();
+    let mainDegree = document.querySelector("#main-temp");
+    let highDegree = document.querySelector("#main-high");
+    let lowDegree = document.querySelector("#main-low");
+
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+
+    mainDegree.innerHTML = Math.round((mainCelsiusTemperature * 9) / 5 + 32);
+    highDegree.innerHTML = Math.round((mainCelsiusHigh * 9) / 5 + 32);
+    lowDegree.innerHTML = Math.round((mainCelsiusLow * 9) / 5 + 32);
+  }
+
+  function mainCelsiusConversion(event) {
+    event.preventDefault();
+    let mainDegreeC = document.querySelector("#main-temp");
+    let highDegreeC = document.querySelector("#main-high");
+    let lowDegreeC = document.querySelector("#main-low");
+
+    fahrenheitLink.classList.remove("active");
+    celsiusLink.classList.add("active");
+
+    mainDegreeC.innerHTML = Math.round(mainCelsiusTemperature);
+    highDegreeC.innerHTML = Math.round(mainCelsiusHigh);
+    lowDegreeC.innerHTML = Math.round(mainCelsiusLow);
+  }
+
+  let mainFahrenheitLink = document.querySelector("#fahrenheit-link");
+  mainFahrenheitLink.addEventListener("click", mainFahrenheitConversion);
+
+  let mainCelsiusLink = document.querySelector("#celsius-link");
+  mainCelsiusLink.addEventListener("click", mainCelsiusConversion);
+}
+
+function displayCurrentForecast(response) {
+  let currentForecast = response.data.list;
+
+  let currentForecastElement = document.querySelector("#forecast");
+
+  let currentForecastHTML = `<div class="row">`;
+  currentForecast.forEach(function (formatDay, index) {
+    let currentHighTemp = Math.round(formatDay.main.temp_max);
+    let currentLowTemp = Math.round(formatDay.main.temp_min);
+    let currentForecastIcon = formatDay.weather[0].main;
+    formatForecast(currentTime);
+
+    function formatForecast(day) {
+      let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+      let currentForecastDay = index;
+      if (currentForecastDay === 0) {
+        currentForecastDay = days[day.getDay() + 1];
+      }
+      if (currentForecastDay === 1) {
+        currentForecastDay = days[day.getDay() + 2];
+      }
+      if (currentForecastDay === 2) {
+        currentForecastDay = days[day.getDay() + 3];
+      }
+
+      if (currentForecastIcon === "Clear") {
+        currentForecastIcon = `<span class="fa-solid fa-sun" id="item2"></span>`;
+      }
+      if (currentForecastIcon === "Clouds") {
+        currentForecastIcon = `<span class="fa-solid fa-cloud-sun" id="item2"></span>`;
+      }
+      if (currentForecastIcon === "Rain") {
+        currentForecastIcon = `<span class="fa-solid fa-cloud-rain" id="item2"></span>`;
+      }
+      if (currentForecastIcon === "Snow") {
+        currentForecastIcon = `<span class="fa-solid fa-snowflake" id="item2"></span>`;
+      }
+
+      if (index < 3) {
+        currentForecastHTML =
+          currentForecastHTML +
+          `<div class="col-4">
+  <h2 class="forecast-day" id="item1">${currentForecastDay}</h2>
+  ${currentForecastIcon}
+  <br />
+  <p class="forecast-temp id="item3">
+   <span id="forecast-temp-high">${currentHighTemp} |</span><span id="forecast-temp-low"> ${currentLowTemp} </span>
+  </p>
+</div>`;
+      }
+    }
+  });
+
+  currentForecastElement.innerHTML = currentForecastHTML;
+}
+
+window.onload = currentLocation();
+
+function forecastLocation() {
+  navigator.geolocation.getCurrentPosition(getFutureForecast);
+}
+
+function getFutureForecast(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "a7083641b7ebf003aec9614bf223ec5f";
+  let forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(forecastApiUrl).then(displayFutureForecast);
+}
+
+function displayFutureForecast(response) {
+  let futureForecast = response.data.list;
+  console.log(futureForecast);
+
+  let futureForecastElement = document.querySelector("#forecast");
+
+  let futureForecastHTML = `<div class="row">`;
+  futureForecast.forEach(function (formatDay, index) {
+    let futureHighTemp = Math.round(formatDay.main.temp_max);
+    let futureLowTemp = Math.round(formatDay.main.temp_min);
+    let futureForecastIcon = formatDay.weather[0].main;
+    formatFutureForecast(currentTime);
+
+    function formatFutureForecast(futureDay) {
+      let futureDays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+      let futureForecastDay = index;
+      console.log();
+
+      if (futureForecastDay === 0) {
+        futureForecastDay = futureDays[futureDay.getDay() + 4];
+      }
+      if (futureForecastDay === 1) {
+        futureForecastDay = futureDays[futureDay.getDay() + 5];
+      }
+      if (futureForecastDay === 2) {
+        futureForecastDay = futureDays[futureDay.getDay() + 6];
+      }
+
+      if (futureForecastIcon === "Clear") {
+        futureForecastIcon = `<span class="fa-solid fa-sun" id="item2"></span>`;
+      }
+      if (futureForecastIcon === "Clouds") {
+        futureForecastIcon = `<span class="fa-solid fa-cloud-sun" id="item2"></span>`;
+      }
+      if (futureForecastIcon === "Rain") {
+        futureForecastIcon = `<span class="fa-solid fa-cloud-rain" id="item2"></span>`;
+      }
+      if (futureForecastIcon === "Snow") {
+        futureForecastIcon = `<span class="fa-solid fa-snowflake" id="item2"></span>`;
+      }
+
+      if (index < 3) {
+        futureForecastHTML =
+          futureForecastHTML +
+          `<div class="col-4">
+  <h2 class="forecast-day" id="item1">${futureForecastDay}</h2>
+  ${futureForecastIcon}
+  <br />
+  <p class="forecast-temp id="item3">
+   <span id="forecast-temp-high">${futureHighTemp} |</span><span id="forecast-temp-low"> ${futureLowTemp} </span>
+  </p>
+</div>`;
+      }
+    }
+  });
+
+  futureForecastElement.innerHTML = futureForecastHTML;
+}
+
+let rightClick = document.querySelector("#right-arrow-link");
+rightClick.addEventListener("click", forecastLocation);
+
 // Search Bar
 
 function cityName(event) {
@@ -248,4 +478,3 @@ celsiusLink.addEventListener("click", celsiusConversion);
 let celsiusTemperature = null;
 let celsiusTemperatureHigh = null;
 let celsiusTemperatureLow = null;
-displayForecast();
